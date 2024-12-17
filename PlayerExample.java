4346a458d51a5f6c7bf3cd86f6e9f9d54ad26ea6
Ian Chen <ic7065@pleasantonusd.net>
2024-12-17 06:44:33 +0000

    og strat

--- PlayerExample.java
+++ PlayerExample.java
@@ -7,10 +7,27 @@ import java.util.*;
  * 
  * You can create additional methods but must have the move method
  * 
+ * common: same, beat enemy last, rand plus same every 3 mvs??
+ * Ian C
+ * y m
+ * r pm
+ * s py
+ * s rm
+ * p ry
+ * p ry
+ * p p
+ * s sy
+ * 
+ * y m
+ * r pm
+ * s py
+ * s rm
+ * p p
+ * s p
  */
 public class PlayerExample implements Player
 {
-    private static String name = "PlayerExample";
+    private static String name = "Probability";
     /**
      * An example of a method - replace this comment with your own
      * You must create some kind of logic of what to play against your opponent...start thinking!
@@ -23,8 +40,31 @@ public class PlayerExample implements Player
      */
     public String move(String [] myMoves, String [] opponentMoves, int myScore, int opponentScore)
     {
-        
-        return " ";
+        int r,p,s;
+        r=p=s=0;
+        for (String mv :opponentMoves){
+            if (mv=="r"){
+                r++;
+            }
+            if (mv=="p"){
+                p++;
+            }
+            if (mv=="s"){
+                s++;
+            }
+        }
+        if (r>p&&r>s){
+            return "p";
+        }
+        else if (p>r&&p>s){
+            return "s";
+        }
+        else if (s>r&&s>p){
+            return "r";
+        }
+        else{
+            return new String[] {"r","p","s"}[new Random().nextInt(3)];
+        }
     }
     /**
      * Returns the name of the player
